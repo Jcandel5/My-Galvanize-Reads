56e83f8025ab6394482be98576695ee17cf00198
Jared Candelaria <jaredcandelaria@Jareds-MacBook-Pro.local>
2018-10-08 10:22:58 -0600

    added body parser and cors

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express')
 const app = express()
-const port = processs.env.PORT || 3000
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
+app.use(bodyParser.json())
+app.use(cors())
+app.get('/', (req,res,next) => {
 
+})
+
+app.listen(port , () => {
+    console.log(`listening on port ${port}`)
+})
